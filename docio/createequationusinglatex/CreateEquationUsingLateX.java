48afb5c536f24373f18b0bd85d73d284ff6b1780
mathankumar.v <mathankumar.v@syncfusion.com>
2024-08-20 19:24:30 +0530

    Align code

--- docio/createequationusinglatex/CreateEquationUsingLateX.java
+++ docio/createequationusinglatex/CreateEquationUsingLateX.java
@@ -10,18 +10,18 @@ public class CreateEquationUsingLateX {
 		WordDocument document = new WordDocument(getDataDir("Create Equation.docx"), FormatType.Docx);		
 		// Get the last section of the document.
 		WSection section = document.getLastSection();
-	    // Set all margins of the page to 72 points.
+		// Set all margins of the page to 72 points.
 		section.getPageSetup().getMargins().setAll((float)72);
 		// Add a new paragraph to the section.
 		IWParagraph paragraph = section.addParagraph();
 		// Add text to the paragraph and set its font size to 28 points.
 		IWTextRange textRange = paragraph.appendText("Mathematical equation");
 		textRange.getCharacterFormat().setFontSize((float)28);
-		 // Center align the paragraph.
+		// Center align the paragraph.
 		paragraph.getParagraphFormat().setHorizontalAlignment(HorizontalAlignment.Center);
-	    // Set the spacing after the paragraph to 12 points.
+		// Set the spacing after the paragraph to 12 points.
 		paragraph.getParagraphFormat().setAfterSpacing((float)12);
-		paragraph=section.addParagraph();
+		paragraph = section.addParagraph();
 		// Append equation to the paragraph.
 		paragraph.appendMath("f\\left(x\\right)={a}_{0}+\\sum_{n=1}^{\\infty}{\\left({a}_{n}\\cos{\\frac{n\\pi{x}}{L}}+{b}_{n}\\sin{\\frac{n\\pi{x}}{L}}\\right)}");
 		//Save the word document.
